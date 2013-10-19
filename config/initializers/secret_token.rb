21af2eab97bc1e8ae71bb8ae979aa09b1f4b2da9
DeVon McWhite <dmcwhite@gmail.com>
2013-10-19 15:24:56 -0800

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Emptydes::Application.config.secret_key_base = '10fff60ce36b21a32b5c87d5653d088052900dca3db6ba8cb75cd34b82e1673704fda5cb3d2498bc1074e3c9df75c299804619d964a0f98bf44868ab987876f1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Emptydes::Application.config.session_store :cookie_store, key: '_emptydes_session'
